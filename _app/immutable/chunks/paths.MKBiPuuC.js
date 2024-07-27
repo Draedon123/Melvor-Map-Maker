3bbc2807d50d74a19e66b6ac86cb1f2377915fba
Draedon <draedon123@gmail.com>
2024-07-27 20:10:11 +1000

    v0.5.2

---
+++ _app/immutable/chunks/paths.MKBiPuuC.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1q82d8w)==null?void 0:s.base)??"/Melvor-Map-Maker";var a;const t=((a=globalThis.__sveltekit_1q82d8w)==null?void 0:a.assets)??e;export{t as a,e as b};
